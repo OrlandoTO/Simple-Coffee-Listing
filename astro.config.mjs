7ca416d676d3ed70b68673a422a12f28abd41e51
OrlandoTO <jose251999leo@gmail.com>
2024-02-27 22:18:39 -0500

    Add configuration for automatic deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://OrlandoTO.github.io',
+  base: '/Simple-Coffee-Listing',
   integrations: [tailwind(), react()]
 });
